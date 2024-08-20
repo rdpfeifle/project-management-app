e1c6dc1529fe87ba6d2dc2be3a65623c61e98d56
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2024-08-20 14:51:50 -0600

    Add some styling to Sidebar and App components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
-    <main className="h-screen">
+    <main className="h-screen my-8">
       <Sidebar />
     </main>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,10 +2,12 @@ import { Button } from "./Button";
 
 export function Sidebar() {
   return (
-    <aside className="bg-black">
-      <h2 className="uppercase font-bold text-2xl text-white">Your projects</h2>
+    <aside className="bg-slate-900 text-slate-50 w-1/3 px-8 py-16 ,md:w-72 rounded-r-xl">
+      <h2 className="mb-8 uppercase font-bold md:text-xl text-slate-200">
+        Your projects
+      </h2>
       <div>
-        <Button className="text-gray-600 bg-gray-300 py-2 px-3 rounded-md">
+        <Button className="text-slate-400 bg-slate-700 py-2 px-4 rounded-md text-xs md:text-base hover:bg-slate-600 hover:text-slate-100">
           + Add Project
         </Button>
       </div>
