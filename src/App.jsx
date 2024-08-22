14fc6b41fe69063e8c445d7a0652ee5d273b0168
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2024-08-22 07:57:31 -0600

    Create and render NoProjectSelected component in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { NewProject } from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <Sidebar />
+      {/* <NewProject /> */}
+      <NoProjectSelected />
     </main>
   );
 }
---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,25 @@
+import noProjectsImg from "../assets/no-projects.png";
+import { Button } from "./Button";
+
+export default function NoProjectSelected() {
+  return (
+    <div className="my-24 text-center w-2/3">
+      <img
+        alt="An empty task list"
+        src={noProjectsImg}
+        className="w-16 h-16 object-contain mx-auto"
+      />
+      <h2 className="mt-4 capitalize text-xl font-bold text-slate-500">
+        No project selected
+      </h2>
+      <p className="mb-4 text-slate-400">
+        Select an existing project or create a new one.
+      </p>
+      <p className="mt-8">
+        <Button className="text-slate-400 bg-slate-700 py-2 px-4 rounded-md text-xs md:text-base hover:bg-slate-600 hover:text-slate-100">
+          Create new project
+        </Button>
+      </p>
+    </div>
+  );
+}
