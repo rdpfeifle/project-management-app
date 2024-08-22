4146ede24f492cba812bc24a282af6e7b2ec7d7f
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2024-08-22 13:31:34 -0600

    Create first draft of Project Details components

---
+++ src/components/ProjectDetails.jsx
@@ -0,0 +1,27 @@
+import { Button } from "./Button";
+
+export function ProjectDetails({ project }) {
+  const { title, description, dueDate } = project;
+
+  const formattedDate = new Date(dueDate).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+
+  return (
+    <div className="w-[35rem] mt-16">
+      <header className="pb-4 mb-4 border-b-2 border-slate-300">
+        <div className="flex items-center justify-between">
+          <h1 className="mb-2 text-3xl font-bold text-slate-600">{title}</h1>
+          <Button className="text-slate-600 hover:text-slate-950">
+            Delete
+          </Button>
+        </div>
+        <p className="mb-4 text-slate-400">{formattedDate}</p>
+        <p className="text-slate-600 whitespace-pre-wrap">{description}</p>
+      </header>
+      TASKS
+    </div>
+  );
+}
