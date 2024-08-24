cb267225b6bbd47e7aeb9acc866918587bc730da
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2024-08-24 16:37:21 -0600

    Clean App.jsx and utilize useContext to access projects and selectedProjectId

--- src/App.jsx
+++ src/App.jsx
@@ -1,130 +1,31 @@
-import { useState } from "react";
-import { v4 as uuid } from "uuid";
+import { useContext } from "react";
 import { NewProject } from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import { Sidebar } from "./components/Sidebar";
 import { ProjectDetails } from "./components/ProjectDetails";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Context } from "./store/context";
 
 function App() {
-  const [projects, setProjects] = useState({
-    selectedProjectId: undefined,
-    projects: [],
-    tasks: [],
-  });
-  const uniqueId = uuid();
-  const smallId = uniqueId.slice(0, 7);
+  const { projects, selectedProjectId } = useContext(Context);
 
-  const { selectedProjectId } = projects;
-
-  const handleSelectProject = (id) => {
-    setProjects((prevState) => {
-      return {
-        ...prevState,
-        selectedProjectId: id,
-      };
-    });
-  };
-
-  const handleStartAddProject = () => {
-    setProjects((prevState) => {
-      return {
-        ...prevState,
-        selectedProjectId: null,
-      };
-    });
-  };
-
-  const handleCancelAddProject = () => {
-    setProjects((prevState) => {
-      return {
-        ...prevState,
-        selectedProjectId: undefined,
-      };
-    });
-  };
-
-  const handleAddProject = (projectData) => {
-    setProjects((prevState) => {
-      const newProject = {
-        ...projectData,
-        id: smallId,
-      };
-      return {
-        ...prevState,
-        selectedProjectId: undefined,
-        projects: [...prevState.projects, newProject],
-      };
-    });
-  };
-
-  const handleDeleteProject = () => {
-    setProjects((prevState) => {
-      return {
-        ...prevState,
-        selectedProjectId: undefined,
-        projects: prevState.projects.filter(
-          (project) => project.id !== prevState.selectedProjectId
-        ),
-      };
-    });
-  };
-
-  const handleAddTask = (text) => {
-    setProjects((prevState) => {
-      const newTask = {
-        text: text,
-        projectId: prevState.selectedProjectId,
-        taskId: smallId,
-      };
-      return {
-        ...prevState,
-        tasks: [...prevState.tasks, newTask],
-      };
-    });
-  };
-
-  const handleDeleteTask = (id) => {
-    setProjects((prevState) => {
-      return {
-        ...prevState,
-        tasks: prevState.tasks.filter((task) => task.taskId !== id),
-      };
-    });
-  };
-
-  const selectedProject = projects.projects.find(
+  const selectedProject = projects.find(
     (project) => project.id === selectedProjectId
   );
 
-  let content = (
-    <ProjectDetails
-      project={selectedProject}
-      onDelete={handleDeleteProject}
-      onAddTask={handleAddTask}
-      onDeleteTask={handleDeleteTask}
-      tasks={projects.tasks}
-    />
-  );
+  let content = <ProjectDetails project={selectedProject} />;
 
   if (selectedProjectId === null) {
-    content = (
-      <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
-    );
+    content = <NewProject />;
   } else if (selectedProjectId === undefined) {
-    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+    content = <NoProjectSelected />;
   }
 
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <Sidebar
-          onStartAddProject={handleStartAddProject}
-          projects={projects.projects}
-          onSelectProject={handleSelectProject}
-          selectedProjectId={projects.selectedProjectId}
-        />
+        <Sidebar />
         {content}
       </main>
       <ToastContainer />
